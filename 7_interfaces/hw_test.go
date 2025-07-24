083ed1a44d0afaf5caa1de4f262688cdd004d4d2
ashinurn <sahiashinurn@gmail.com>
2025-07-24 09:54:18 +0300

    add di containers basic solution

---
+++ 7_interfaces/hw_test.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestDIContainer(t *testing.T) {
+	container := NewContainer()
+
+	container.RegisterType("UserService", func() any {
+		return &UserService{}
+	})
+	container.RegisterType("MessageService", func() any {
+		return &MessageService{}
+	})
+
+	userService1, err := container.Resolve("UserService")
+	assert.NoError(t, err)
+	userService2, err := container.Resolve("UserService")
+	assert.NoError(t, err)
+
+	u1 := userService1.(*UserService)
+	u2 := userService2.(*UserService)
+	assert.False(t, u1 == u2)
+
+	messageService, err := container.Resolve("MessageService")
+	assert.NoError(t, err)
+	assert.NotNil(t, messageService)
+
+	paymentService, err := container.Resolve("PaymentService")
+	assert.Error(t, err)
+	assert.Nil(t, paymentService)
+}
